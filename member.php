5b1116980d1f4ed61ab968b023af34ab2180a47b
unwabuisi <unwabuisi@utexas.edu>
2017-09-27 16:15:03 -0500

    yes it will

--- member.php
+++ member.php
@@ -62,7 +62,7 @@
             <td><input type="button" id="addmorePOIbutton" value="Add" onclick="insRow()"/></td>
         </tr> -->
         <tr>
-		<form action="member.php" method="post">
+		<form action="memberAdd.php" method="post">
             <td>
         		<input type="text" name="Name" placeholder="First & Last Name">
             </td>
@@ -76,23 +76,6 @@
         </form>
         </tr>
     </table>
-
-    <?php
-	    $name = $_POST["Name"]; //You have to get the form data
-	    $attending = $_POST["attendance"];
-	    $filename = 'RSVPList.txt';
-
-	    $fp = fopen($filename, 'a+'); //Open your .txt file
-	    fwrite($fp , $name . ":" . $attending . "\n"); //Now lets write it in there
-	    
-	    fclose($fp); //Finally close our .txt
-
-	?>
-
-
-
-
-
 
     <script type="text/javascript">
       function deleteRow(row)
